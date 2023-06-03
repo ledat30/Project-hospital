44d10766c17b8bc9e8acc6be3c9a6b994551026b
ledat30 <92835880+ledat30@users.noreply.github.com>
2023-06-03 08:22:25 +0700

    OnchangLogin

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+import * as actions from "../../store/actions";
+import './Login.scss';
+import { FormattedMessage } from 'react-intl';
+
+import adminService from '../../services/adminService';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            isShowPassword: false
+        }
+    }
+
+    handleOnchangUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+
+    handleOnchangPassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleLogin = () => {
+        console.log('username:', this.state.username, "password:", this.state.password)
+    }
+
+    handleShowHidePasswprd = () => {
+        this.setState({
+            isShowPassword: !this.state.isShowPassword
+        })
+    }
+
+    render() {
+        return (
+            <div className='login-background'>
+                <div className='login-container'>
+                    <div className='login-content row'>
+                        <div className='col-12 text-login'>Login</div>
+                        <div className='col-12 form-group login-input'>
+                            <label>UserName : </label>
+                            <input type='text' className='form-control' placeholder='Enter your username'
+                                value={this.state.username}
+                                onChange={(event) => { this.handleOnchangUsername(event) }}
+                            />
+                        </div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Password :</label>
+                            <div className='custom-input-password'>
+                                <input type={this.state.isShowPassword ? 'text' : 'password'} className='form-control' placeholder='Enter your password'
+                                    onChange={(event) => { this.handleOnchangPassword(event) }}
+                                />
+                                <span
+                                    onClick={() => { this.handleShowHidePasswprd() }}
+                                >
+                                    <i className={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
+                                </span>
+
+                            </div>
+                        </div>
+                        <div className='col-12 ' >
+                            <button className='btn-login' onClick={() => { this.handleLogin() }}>Login</button>
+                        </div>
+                        <div className='col-12'>
+                            <span className='forgot-password'>Forgot your password?</span>
+                        </div>
+                        <div className='col-12 text-center mt-3'>
+                            <span className='text-other-login'>Or login with</span>
+                        </div>
+                        <div className='col-12 social-login'>
+                            <i className='fab fa-google-plus-g google'></i>
+                            <i className='fab fa-facebook-f facebook'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        navigate: (path) => dispatch(push(path)),
+        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
