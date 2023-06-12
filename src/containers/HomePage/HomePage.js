7699da154f3feb506942da12a7ad0a00715abd45
ledat30 <92835880+ledat30@users.noreply.github.com>
2023-06-12 18:48:41 +0700

    slide-carousel

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HeaderHome from './HeaderHome';
+import Specialty from './Section/Specialty';
 
 class HomePage extends Component {
 
@@ -8,6 +9,10 @@ class HomePage extends Component {
         return (
             <div>
                 <HeaderHome />
+                <Specialty />
+                <div style={{ height: '300px' }}>
+
+                </div>
             </div>
         );
     }
