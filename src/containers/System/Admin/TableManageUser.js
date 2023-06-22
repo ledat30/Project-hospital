8e8e03b5957d13573113204cd9f8feec0a3c721e
ledat30 <92835880+ledat30@users.noreply.github.com>
2023-06-22 08:45:58 +0700

    install and use markdown editor -host image free with github

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -4,6 +4,26 @@ import { connect } from 'react-redux';
 import './TableManageUser.scss';
 import * as actions from '../../../store/actions';
 
+
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text);
+}
+
+
+
+
 class TableManageUser extends Component {
 
     constructor(props) {
@@ -35,34 +55,38 @@ class TableManageUser extends Component {
     render() {
         let arrUser = this.state.userRedux;
         return (
-            <table id='TableManageUser'>
-                <tbody>
-                    <tr>
-                        <th>FullName</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                        <th>Phone</th>
-                        <th>Action</th>
-                    </tr>
-                    {arrUser && arrUser.length > 0 && arrUser.map((item, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{item.fullName}</td>
-                                <td>{item.email}</td>
-                                <td>{item.address}</td>
-                                <td>{item.phonenumber}</td>
-                                <td>
-                                    <button className='btn-edit'
-                                        onClick={() => this.handleEditUser(item)}><i className='fas fa-pencil-alt'></i></button>
-                                    <button className='btn-delete'
-                                        onClick={() => this.handleDeleteUser(item)}>
-                                        <i className='fas fa-trash'></i></button>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
+            <>
+                <table id='TableManageUser'>
+                    <tbody>
+                        <tr>
+                            <th>FullName</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                            <th>Phone</th>
+                            <th>Action</th>
+                        </tr>
+                        {arrUser && arrUser.length > 0 && arrUser.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.fullName}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.address}</td>
+                                    <td>{item.phonenumber}</td>
+                                    <td>
+                                        <button className='btn-edit'
+                                            onClick={() => this.handleEditUser(item)}><i className='fas fa-pencil-alt'></i></button>
+                                        <button className='btn-delete'
+                                            onClick={() => this.handleDeleteUser(item)}>
+                                            <i className='fas fa-trash'></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+
+                <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+            </>
         );
     }
 
