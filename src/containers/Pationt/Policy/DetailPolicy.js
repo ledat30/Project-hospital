0268a3493a1b8901a7b9c8dffd4be4a9048aa081
ledat30 <92835880+ledat30@users.noreply.github.com>
2023-08-02 20:28:47 +0700

    them danh muc vào detailPolicy(web)

--- src/containers/Pationt/Policy/DetailPolicy.js
+++ src/containers/Pationt/Policy/DetailPolicy.js
@@ -4,17 +4,19 @@ import { FormattedMessage } from 'react-intl';
 import './DetailPolicy.scss';
 import HeaderHome from '../../HomePage/HeaderHome';
 import HomeFooter from '../../HomePage/HomeFooter';
-import { getDetailPolicyById } from '../../../services/userService';
+import { getDetailPolicyById, getAllPolicy } from '../../../services/userService';
 import _ from 'lodash';
 import { LANGUAGES } from '../../../utils';
-
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
 class DetailPolicy extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            dataDetailPolicy: {}
+            dataDetailPolicy: {},
+            dataPolicy: []
         }
     }
 
@@ -28,16 +30,42 @@ class DetailPolicy extends Component {
                 dataDetailPolicy: res.data
             })
         }
+
+        let policy = await getAllPolicy();
+        if (policy && policy.errCode === 0) {
+            this.setState({
+                dataPolicy: policy.data ? policy.data : []
+            })
+        }
     }
 
     async componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.props.language !== prevProps.language) {
+            let id = this.props.match.params.id;
+            let res = await getDetailPolicyById({
+                id: id
+            });
+
+            this.setState({
+                dataDetailPolicy: res.data
+            })
+        }
+
+        if (this.props.match.params.id !== prevProps.match.params.id) {
+            let id = this.props.match.params.id;
+            let res = await getDetailPolicyById({
+                id: id
+            });
+
+            this.setState({
+                dataDetailPolicy: res.data
+            })
         }
     }
 
 
     render() {
-        let { dataDetailPolicy } = this.state;
+        let { dataDetailPolicy, dataPolicy } = this.state;
         let { language } = this.props;
         return (
             <div className='detail-specialty-container'>
@@ -57,6 +85,23 @@ class DetailPolicy extends Component {
 
                         }
                     </div>
+
+                    <div className='container-tag'>
+                        <div className='title-tag'><FormattedMessage id={'patient.handbook.category'} /></div>
+                        <div className='containers'>
+                            {dataPolicy && dataPolicy.length > 0 && dataPolicy.map((item, index) => {
+                                return (
+                                    <Link to={`/detail-policy/${item.id}`}
+                                        onClick={() => window.scrollTo(0, 0)}
+                                    >
+                                        <div className='tag_category' key={index}>
+                                            # {language === LANGUAGES.VI ? item.nameVI : item.nameEN}
+                                        </div>
+                                    </Link>
+                                )
+                            })}
+                        </div>
+                    </div>
                 </div>
                 <HomeFooter />
             </div >
@@ -75,4 +120,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(DetailPolicy);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(DetailPolicy));
